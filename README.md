f5096ce73f8c85b347c2bf13a50eb2f4fa51f7c9
3n3a <46775561+3n3a@users.noreply.github.com>
2025-06-09 13:18:08 +0200

    add new variants

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ static, generative, .... it's all art
 
 * [artsy rectangles](random_rects/)
 * [artsy rectangles in motion](random_rects_motion/)
+* [artsy circles in motion](random_rects_motion/)
+* [artsy shapes in motion](random_shapes_motion/)
 * [hacker art](hacker_art/)
-* [camera in html](camera_html/)
+
---
+++ random_shapes_motion/script.js
@@ -0,0 +1,101 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getRandomIntInRange(min, max) {
+    min = Math.ceil(min);
+    return Math.floor(Math.random() * (Math.floor(max) - min + 1)) + min;
+}
+
+class Canvas {
+    constructor(width = window.innerWidth, height = window.innerHeight) {
+        this.UPPER_BLOCK_LIMIT = width * height * 10;
+        this.UPPER_BLOCK_SIZE_LIMIT_DIVISION = 5;
+
+        this.width = width;
+        this.height = height;
+        this.canvas = null;
+        this.rects = [];
+        this.nextBidirectionalRange = 10;
+        this.iterationDelay = 180;
+
+        this.createCanvas();
+        this.createGenesisShapes();
+        this.drawShapes();
+
+        setInterval(() => {
+            this.createNextShapes();
+            this.canvas.clearRect(0, 0, canvas.width, canvas.height);
+            this.drawShapes();
+        }, this.iterationDelay);
+    }
+
+    createCanvas() {
+        let canvas = document.createElement("canvas");
+        canvas.id = "canvas";
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.canvas = canvas.getContext("2d");
+        document.body.appendChild(canvas);
+    }
+
+    createGenesisShapes() {
+        for (let i = 0; i < getRandomInt(this.UPPER_BLOCK_LIMIT); i++) {
+            this.rects[i] = {
+                color: `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(
+                    255
+                )})`,
+                x: getRandomInt(this.width),
+                y: getRandomInt(this.height),
+                width: getRandomInt(this.width) / this.UPPER_BLOCK_SIZE_LIMIT_DIVISION,
+                height:
+                    getRandomInt(this.height) / this.UPPER_BLOCK_SIZE_LIMIT_DIVISION,
+            };
+        }
+    }
+
+    createNextShapes() {
+        for (let i = 0; i < this.rects.length; i++) {
+            this.rects[i] = {
+                color: this.rects[i].color,
+                x: getRandomIntInRange(
+                    this.rects[i].x - this.nextBidirectionalRange,
+                    this.rects[i].x + this.nextBidirectionalRange
+                ),
+                y: getRandomIntInRange(
+                    this.rects[i].y - this.nextBidirectionalRange,
+                    this.rects[i].y + this.nextBidirectionalRange
+                ),
+                width: this.rects[i].width,
+                height: this.rects[i].height,
+            };
+        }
+    }
+
+    drawShapes() {
+        for (let i = 0; i < this.rects.length; i++) {
+            if (i % 2 == 0) {
+                this.canvas.fillStyle = this.rects[i].color;
+                this.canvas.fillRect(
+                    this.rects[i].x,
+                    this.rects[i].y,
+                    this.rects[i].width,
+                    this.rects[i].height
+                );
+            } else {
+                this.canvas.beginPath();
+                this.canvas.fillStyle = this.rects[i].color;
+                this.canvas.arc(
+                    this.rects[i].x,
+                    this.rects[i].y,
+                    this.rects[i].width / 2,
+                    0,
+                    2 * Math.PI
+                );
+                this.canvas.fill();
+            }
+        }
+    }
+}
+
+let c = new Canvas();
